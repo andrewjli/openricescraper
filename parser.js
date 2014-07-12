bf8697433f63884c2e9a631c4d5bf4e206bf87f8
Andrew Li <8610177+andrewjli@users.noreply.github.com>
2014-07-12 20:06:22 +0800

    Working parser

--- parser.js
+++ parser.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var cheerio = require("cheerio");
 var esprima = require("esprima")
+var async = require("async")
 
 function start(param, response) {
 	//console.log(param);
@@ -14,9 +15,7 @@ function start(param, response) {
 
 		var base = "/english/restaurant/sr1.htm?"
 		var suffix = "&z=10&wxh=5x5&currentlocation=1"
-
 		var queryurl = base + "x=" + params[0][1] + "&y=" + params[1][1] + suffix;
-
 		var options = {
 			host: "www.openrice.com",
 			headers: {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13' },
@@ -30,19 +29,23 @@ function start(param, response) {
 			});
 
 			result.on("end", function() {
-				//console.log(data)
-
 				var $ = cheerio.load(data);
 
-				response.writeHead(200, {"Content-Type": "text/plain"});
+				var res_arr = [];
 
-				var restaurants = $("a.sr1_title").each(function() {
+				$("a.sr1_title").each(function() {
 					var link = $(this)
 					var text = link.text();
 					var href = link.attr("href");
 
-					response.write(text + " -> " + href + "\n");
+					res_arr.push({ 'name': text, 'link': href })
+				});
 
+
+
+				var counter = 0
+				async.each(res_arr, function(restaurant, callback) {
+					href = restaurant.link;
 					var option = {
 						host: "www.openrice.com",
 						headers: {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13' },
@@ -58,14 +61,23 @@ function start(param, response) {
 						result.on("end", function() {
 							var $$ = cheerio.load(data);
 							var coords = esprima.parse($$(".pg_main > script").first().text());
-							var x = coords.body[0].expression.right.value;
-							var y = coords.body[1].expression.right.value;
-							console.log(x + "," + y);
+							restaurant.x = coords.body[0].expression.right.value;
+							restaurant.y = coords.body[1].expression.right.value;
+							//console.log(restaurant.x + "," + restaurant.y);
+							counter++;
+							callback();
 						});
 					});
-				});
+				}, function (err) {
+					if(err) return next(err);
+					//console.log(counter)
 
-                response.end();
+					//console.log(res_arr);
+
+					response.writeHead(200, {"Content-Type": "text/plain"});
+					response.write(JSON.stringify(res_arr));
+					response.end();
+				});
 			});
 		});
 	}
