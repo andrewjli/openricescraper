f73d50202c29f92735e4fcc141c586702ac68436
Andrew Li <8610177+andrewjli@users.noreply.github.com>
2014-07-12 17:00:43 +0800

    New things

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var http = require("http");
 var url = require("url");
+var parser = require("./parser");
 
 function start() {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         var param = url.parse(request.url).search;
-        console.log("Request received for " + pathname)
-        
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
+        if(pathname != "/favicon.ico") {
+            console.log("Request received for " + pathname)
+
+            parser.start(param, response);
+        }
     }
     
     http.createServer(onRequest).listen(8888);
