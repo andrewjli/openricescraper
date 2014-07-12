9c810ea882a2dfcd92dd0f28eed424c0a15015c2
Andrew Li <8610177+andrewjli@users.noreply.github.com>
2014-07-12 15:22:53 +0800

    Removed useless lines

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var url = require("url");
 
 function start() {
     function onRequest(request, response) {
-        /* Parse URL */
         var pathname = url.parse(request.url).pathname;
         var param = url.parse(request.url).search;
         console.log("Request received for " + pathname)
@@ -14,7 +13,6 @@ function start() {
     }
     
     http.createServer(onRequest).listen(8888);
-    //http.createServer(onRequest).listen(process.env.PORT, process.env.IP);
     console.log("Server started");
 }
 
